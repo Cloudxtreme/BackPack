273821fadb9839b0a2e88b507ae19fe7a7038985
Hech <tryctor@gmail.com>
2015-02-11 00:11:16 -0800

    update

--- app/views.py
+++ app/views.py
@@ -1,10 +1,26 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from flask import request
+
+users = {'test:test'}
 
 @app.route('/')
-@app.route('/index')
+@app.route('/home')
 def index():
-  user= {'nickname': 'GreatLeader'}
+  user= {'nickname': ''}
   return render_template('ion/index.html',
                         title='GottaBack - Home',
                         user=user)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  error = None
+  if request.method == 'POST':
+    if valid_login(request.form['username'],
+                  request.form['password']):
+      return log_the_user_in(request.form['username'])
+    else:
+      error = 'Invalid username/password'
+  return render_template('ion/login.html',
+                        title='GottaBack - Log In',
+                        ferror=error)
