edda641d58d13fc8d26a63123d3b5f743ebc072e
Hech <tryctor@gmail.com>
2015-02-13 14:13:20 -0800

    comment incase of future errors

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy()
+db.init_app(app)
 
-from app import views
+#Leave at bottom for no conflicts
+from app import views   
