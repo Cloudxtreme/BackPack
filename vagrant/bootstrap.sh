0b2cd371e18e064d8db587230f9eee45eb79b4e7
Christian Cruz <ccruz006@cs.ucr.edu>
2015-03-02 23:13:28 -0800

    vagrant provision script

---
+++ vagrant/bootstrap.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+HOST="`hostname`"
+SERVHOST="serv"
+SERVIP="172.25.0.2"
+CLIENTS=("172.25.0.3")
+REPO="epel-release-6-8.noarch.rpm"
+PREDEST="root@"
+
+echo "-----starting duplicity install-----"
+if [ ! -e "/var/www/${REPO}" ]
+then 
+	cd /var/www/
+	sudo wget "http://download.fedoraproject.org/pub/epel/6/x86_64/${REPO}" 
+fi
+cp "/var/www/${REPO}" "/usr/local/src/${REPO}" 
+cd /usr/local/src/
+sudo rpm -ivh "/usr/local/src/${REPO}"
+
+sudo yum install -y duplicity rsync gpg python python-devel python-pip sshpass
+echo "-----finished duplicity install-----"
+
+if [ "${HOST}" == "${SERVHOST}" ]
+then 
+	echo "-----starting ssh permitions-----"
+
+	if [ ! -e "/root/.ssh/id_rsa" ]
+	then
+		if [ ! -d "/root/.ssh/" ]
+		then
+			mkdir /root/.ssh
+		fi
+		cd /root/.ssh
+		ssh-keygen -f id_rsa -t rsa -N ''
+		sudo yum install -y sshpass
+	fi
+
+	if [ -e "/root/.bashrc" ]
+	then
+		mv /root/.bashrc /root/.bashrc.bak
+	fi
+	
+	for i in "${CLIENTS[@]}"
+	do
+		D=${PREDEST}${i} 
+		sshpass -p 'vagrant' ssh-copy-id -i /root/.ssh/id_rsa $D
+	done
+	mv /root/.bashrc.bak /root/.bashrc
+	echo "-----finished ssh permitions-----"
+fi
